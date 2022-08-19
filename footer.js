de0681e26b4367621560c04ad37f62dfbbe5423f
36_Ansh Yadav <anshyadav182004@gmail.com>
2022-08-19 16:24:10 +0530

    Website

---
+++ footer.js
@@ -0,0 +1,28 @@
+let appFooter = `
+<div class="footer p-5">
+<h4 class="h4"><span class="badge bg-primary">CONTACT US</span></h4>
+    <hr style="height:3px; color:black;">
+<table >
+    <tr>
+        <td><img src="imap.png" height="20px">|</td>
+    <td>xxxxxxxxxxxxxxxxxxxxx Gujarat,India</td>
+    </tr>
+    <tr>
+        <td>📞|</td>
+        <td>xxxxxxxxxx</td>
+    </tr>
+    <tr>
+        <td>📧| </td>
+        <td>xxxxxx@gmail.ac.in</td>
+    </tr>
+</table>
+<br><br>
+<h4 class="h4"><span class="badge pt-2 bg-primary">CONNECT WITH US</span></h4>
+<hr style="height:3px; color:black;">
+<a href="#"><img class="rounded-circle" id="img" src="fbook.png"></a>
+<a href="#"><img class="rounded-circle" id="img" src="insta.jfif"></a>
+<a href="#"><img class="rounded-circle" id="img" src="map.jfif"></a>
+</div>
+`;
+document.getElementById("footer").innerHTML = appFooter;
+
